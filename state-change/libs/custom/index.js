7abf7605181ded5c6f278c74f7fa57991bb4ffed
QuinntyneBrown <quinntynebrown@gmail.com>
2016-02-25 22:43:01 -0500

    remove unused js

--- state-change/libs/custom/index.js
+++
@@ -1,6 +0,0 @@
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-exports.addOrUpdate = angular.injector(['addOrUpdate']).get("addOrUpdate");
-__export(require("./pluckOut"));
-__export(require("./responsivir"));
